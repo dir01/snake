5d47c9ec1618f3577e4f39e1f809aa7dd379c5a0
Andrew Zhu <dir01@dir01.org>
2017-04-03 21:33:31 +0300

    Refactored core game tests

--- src/core/game.test.js
+++ src/core/game.test.js
@@ -22,35 +22,42 @@ describe('Game', () => {
         expect(game.headPosition).to.deep.equal([1, 0]);
     });
 
-    it('moves one cell up and gameover', () => {
-        game.direction = 'up';
-        game.move();
-        expect(game.headPosition).to.deep.equal([1, -1]);
-        expect(game.isOver).to.be.true;
+    describe('game over', () => {
+        let game = new Game({width: 2, height: 2});
+
+        it('moves beyond upper edge', () => {
+            game.headPosition = [1, 0];
+            game.direction = 'up';
+            game.move();
+            expect(game.headPosition).to.deep.equal([1, -1]);
+            expect(game.isOver).to.be.true;
+        });
+
+        it('moves beyond left edge', () => {
+            game.headPosition = [0, 0];
+            game.direction = 'left';
+            game.move();
+            expect(game.headPosition).to.deep.equal([-1, 0]);
+            expect(game.isOver).to.be.true;
+        })
+
+        it('moves beyond right edge', () => {
+            game.headPosition = [game.width - 1, 0];
+            game.direction = 'right';
+            game.move();
+            expect(game.headPosition).to.deep.equal([game.width, 0]);
+            expect(game.isOver).to.be.true;
+        });
+
+        it('moves beyond bottom edge', () => {
+            game.headPosition = [0, game.height - 1];
+            game.direction = 'down';
+            game.move();
+            expect(game.headPosition).to.deep.equal([0, game.height]);
+            expect(game.isOver).to.be.true;
+        });
     });
 
-    it('moves beyond left edge and game over', () => {
-      game.headPosition = [0,0];
-      game.direction = 'left';
-      game.move();
-      expect(game.headPosition).to.deep.equal([-1, 0]);
-      expect(game.isOver).to.be.true;
-    })
 
-    it('moves beyond right edge and game over', () => {
-      game.headPosition = [game.width - 1,0];
-      game.direction = 'right';
-      game.move();
-      expect(game.headPosition).to.deep.equal([game.width, 0]);
-      expect(game.isOver).to.be.true;
-    })
-
-    it('moves beyond bottom edge and game over', () => {
-      game.headPosition = [0,game.height - 1];
-      game.direction = 'down';
-      game.move();
-      expect(game.headPosition).to.deep.equal([0, game.height]);
-      expect(game.isOver).to.be.true;
-    })
 });
 
